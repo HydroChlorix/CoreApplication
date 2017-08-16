ce8e1337d7817308fc8aba807d367ca533197068
Ekkapong <ekkapong@dataroot.asia>
2017-08-16 14:48:41 +0700

    Add dotnet core web project

--- Web.Mvc/Controllers/HomeController.cs
+++ Web.Mvc/Controllers/HomeController.cs
@@ -1,29 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Web.Mvc.Controllers
+namespace Web_Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult Error()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
             return View();
         }
     }
